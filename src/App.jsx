3022a49936954ca561c50bb4872eda7e591cddf9
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-03-13 12:34:42 +0200

    Added:Section Home and Section Brand

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Brands from "./components/pages/Brands";
+import Home from "./components/pages/Home";
+
 function App() {
-  return <></>;
+  return <>
+  <Home/>
+  <Brands/>
+  </>;
 }
 
 export default App;
